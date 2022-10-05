bba49996dd80e55c0b86e0504e4cbf8448a605ec
Kristína Mačičková <@macickova>
2022-10-05 10:14:05 +0200

    edit brackets

--- hanoi.c
+++ hanoi.c
@@ -2,18 +2,16 @@
 
 void hanoi(int n, int a, int b)
 {
-  if (n == 1) 
+  if (n == 1) {
     printf("presun kamen 1 z %d na %d\n", a, b);
-  else
-  {
+  } else {
     hanoi(n - 1, a, 6 - a - b);
     printf("presun kamen %d z %d na %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
 
-int main()
-{
+int main() {
   int n = 4;
 
   hanoi(n, 1, 2);
